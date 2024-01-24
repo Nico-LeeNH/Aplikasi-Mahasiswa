74a14246c110a69694d12ce2bba8199c3e212cf7
ocin14 <new.nikolee2007@gmail.com>
2024-01-24 14:09:15 +0700

    edit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,18 +14,25 @@ class AuthController extends Controller
     }
     public function login(Request $request)
 {
+    $request->validate([
+        'email'=>'required|string|email|',
+        'password'=>'required|string',
+    ]);
 $credentials = $request->only('email', 'password');
-
-if (!$token = JWTAuth::attempt($credentials)) {
-return response()->json(['error' => 'Unauthorized'], 401);
-}
+$token = JWTAuth::attempt($credentials);
+        
+        if (!$token) {
+            return response()->json([
+                'message' => 'Unauthorized',
+            ], 401);
+        }
 
 return response()->json(['token' => $token]);
 }
 
 public function signup(Request $request)
 {
-$user = new User([
+$user = User::create([
 'name' => $request->name,
 'email' => $request->email,
 'password' => Hash::make($request->password),
